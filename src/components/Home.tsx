9293aff4a8705b09ea9d89e670eca880d579dc3f
Takuro Wada <takuro.wada6@gmail.com>
2020-12-07 16:49:13 +0900

    add firebaseAuthを使用してログイン/新規登録機能を実装 #1

---
+++ src/components/Home.tsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+import { db } from "../firebase";
+import styles from "./Home.module.css";
+
+const Home: React.FC = () => {
+  return <div className={styles.home}>Home</div>;
+};
+
+export default Home;
