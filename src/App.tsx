fa1ecf5a441779379e4ad3386a092dd3ea013877
Takuro Wada <takuro.wada6@gmail.com>
2020-12-08 10:21:10 +0900

    add 各ページへのルーティングを設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React, { useEffect } from "react";
 import "./assets/reset.css";
 import styles from "./assets/App.module.scss";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { selectUser, login, logout } from "./features/userSlice";
 import { auth } from "./firebase";
 import Home from "./components/Home";
+import List from "./components/List";
+import Register from "./components/Register";
+import Data from "./components/Data";
+import Setting from "./components/Setting";
 import Auth from "./components/Auth";
+import Header from "./components/Header";
 
 const App: React.FC = () => {
   const user = useSelector(selectUser);
@@ -32,13 +38,20 @@ const App: React.FC = () => {
 
   return (
     <>
-      {user.uid ? (
-        <div className={styles.app}>
-          <Home />
-        </div>
-      ) : (
-        <Auth />
-      )}
+      <Router>
+        {user.uid ? (
+          <div className={styles.app}>
+            <Header />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/list" component={List} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/data" component={Data} />
+            <Route exact path="/setting" component={Setting} />
+          </div>
+        ) : (
+          <Auth />
+        )}
+      </Router>
     </>
   );
 };
