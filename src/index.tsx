e53d23b93b4b0a35e830b30ac55a35e82e203557
Takuro Wada <takuro.wada6@gmail.com>
2020-12-08 19:11:57 +0900

    fix 共通スタイルの定義 #14

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./assets/reset.css";
+import "./assets/base.css";
 import "./index.css";
 import App from "./App";
 import { store } from "./app/store";
