f0bf2802629aaa0debe3612d0e5accb2292b87c8
Takuro Wada <takuro.wada6@gmail.com>
2020-12-18 19:04:23 +0900

    create おすすめガジェット動画ページの作成 #65

---
+++ src/components/RecommendedVideos.tsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import styles from "../assets/scss/RecommendedVideos.module.scss";
+import NoImage from "../assets/images/noimage.png";
+import axios from "axios";
+require("dotenv").config();
+const YOUTUBE_API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
+
+const RecommendedVideos: React.FC = () => {
+  const [videos, setVideos] = React.useState<any[]>([]);
+  const [keyword, setKeyWord] = React.useState("ガジェット");
+
+  useEffect(() => {
+    const url = `https://www.googleapis.com/youtube/v3/search?type=video&part=snippet&q=${keyword}&maxResults=3&key=${YOUTUBE_API_KEY}`;
+
+    axios
+      .get(url)
+      .then((response) => {
+        setVideos(response.data.items);
+      })
+      .catch(() => {
+        console.log("通信に失敗しました");
+      });
+  }, []);
+
+  console.log(videos);
+  const url = "https://www.youtube.com/embed/";
+
+  return (
+    <div className={styles.video}>
+      <h1 className={styles.title}>おすすめガジェット動画</h1>
+
+      <div style={{ margin: "20px", textAlign: "center" }}>
+        {videos.map((video) => (
+          <div className={styles.video_card}>
+            <iframe
+              title={video.id.title}
+              id="ytplayer"
+              width="640"
+              height="360"
+              src={url + video.id.videoId}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default RecommendedVideos;
