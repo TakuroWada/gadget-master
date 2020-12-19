f6e3d3c2206f2c69cac098e70b8669e9e84e6646
Takuro Wada <takuro.wada6@gmail.com>
2020-12-19 14:30:55 +0900

    fix 再度ビルド確認 #78

--- src/components/RecommendedVideos.tsx
+++ src/components/RecommendedVideos.tsx
@@ -1,16 +1,16 @@
-import React /* , { useState, useEffect } */ from "react";
-// import styles from "../assets/scss/RecommendedVideos.module.scss";
-// import NoImage from "../assets/images/noimage.png";
-// import axios from "axios";
-// require("dotenv").config();
+import React, { useState, useEffect } from "react";
+import styles from "../assets/scss/RecommendedVideos.module.scss";
+import NoImage from "../assets/images/noimage.png";
+import axios from "axios";
+require("dotenv").config();
 // const YOUTUBE_API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 
 const RecommendedVideos: React.FC = () => {
-  /* const [videos, setVideos] = React.useState<any[]>([]);
+  const [videos, setVideos] = React.useState<any[]>([]);
   const [keyword, setKeyWord] = React.useState("ガジェット");
 
   useEffect(() => {
-    const url = `https://www.googleapis.com/youtube/v3/search?type=video&part=snippet&q=${keyword}&maxResults=3&key=${YOUTUBE_API_KEY}`;
+    /*  const url = `https://www.googleapis.com/youtube/v3/search?type=video&part=snippet&q=${keyword}&maxResults=3&key=${YOUTUBE_API_KEY}`;
 
     axios
       .get(url)
@@ -19,12 +19,12 @@ const RecommendedVideos: React.FC = () => {
       })
       .catch(() => {
         console.log("通信に失敗しました");
-      });
+      }); */
   }, []);
 
   console.log(videos);
   const url = "https://www.youtube.com/embed/";
- */
+
   return <p>test</p>;
 
   /*  (
