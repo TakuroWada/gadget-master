282126566f4d6e3439bf0309c181d037d76525e4
Takuro Wada <takuro.wada6@gmail.com>
2020-12-20 15:34:05 +0900

    fix ヘッダーのデザイン修正 #98

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,12 +4,14 @@ import { selectUser } from "../features/userSlice";
 import styles from "../assets/scss/Header.module.scss";
 import Nav from "./Nav";
 import { Avatar } from "@material-ui/core";
+import MainTitle from "../assets/images/maintitle.png";
 
 const Header: React.FC = () => {
   const user = useSelector(selectUser);
   return (
     <header className={styles.header}>
       <Avatar className={styles.avatar} src={user.photoUrl} />
+      <img src={MainTitle} className={styles.title} alt="Gadget Master" />
       <Nav />
     </header>
   );
