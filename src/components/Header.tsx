c57d9edb7ed903b3012de968f27150b427155455
Takuro Wada <takuro.wada6@gmail.com>
2020-12-21 12:36:48 +0900

    fix ヘッダータイトルをHOMEへのリンクにする #105

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,7 @@ import { selectUser } from "../features/userSlice";
 import styles from "../assets/scss/Header.module.scss";
 import Nav from "./Nav";
 import { Avatar } from "@material-ui/core";
+import { Link } from "react-router-dom";
 import MainTitle from "../assets/images/maintitle.png";
 
 const Header: React.FC = () => {
@@ -11,7 +12,9 @@ const Header: React.FC = () => {
   return (
     <header className={styles.header}>
       <Avatar className={styles.avatar} src={user.photoUrl} />
-      <img src={MainTitle} className={styles.title} alt="Gadget Master" />
+      <Link to="/">
+        <img src={MainTitle} className={styles.title} alt="Gadget Master" />
+      </Link>
       <Nav />
     </header>
   );
